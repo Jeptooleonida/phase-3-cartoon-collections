c99fc01d831523b248ae8e808f8051e5624d6acd
github_Jeptooleonida <leonida.jeptoo@student.moringaschool.comgit config --global user.name Jeptooleonidagit config --global user.email leonida.jeptoo@student.moringaschool.com>
2022-08-22 05:02:53 +0300

    commit tree 56a4d66251c9bbe86ec78f18e35b096051985647

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,38 @@
-def roll_call_dwarves# code an argument here
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+
+def roll_call_dwarves array
   # Your code here
+  array.each.with_index {|name, idx| puts "#{idx + 1}. #{name}"}
+
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet array
   # Your code here
+  array.map {|name| "#{name.capitalize}!"}
+
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls array
   # Your code here
+  new_array = array.select {|call| call.length > 4 }
+  if new_array == []
+    return false
+  else
+    return true
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese array
+  
   # the array below is here to help
+  new_value = nil
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.each {
+    |item|
+    cheese_types.each { 
+      |type| item == type ? new_value = item : nil
+    }
+  }
+  new_value    
 end
+
